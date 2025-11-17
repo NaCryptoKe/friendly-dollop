4e1affe021b0126693a90c63faed36e0e8c435ca
NaCryptoKe <NahomCryptoKetema@gmail.com>
2025-11-18 01:04:23 +0300

    work

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { google } from "googleapis";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 const app = express();
 const upload = multer({ dest: "uploads/" });
